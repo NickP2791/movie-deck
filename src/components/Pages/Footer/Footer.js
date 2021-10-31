e353b3f0466a1cdb3968716df9d2b3cde522d0f9
NickP2791 <admin@roxanick.com>
2021-10-31 16:39:43 -0500

    fix logo on safari

--- src/components/Pages/Footer/Footer.js
+++ src/components/Pages/Footer/Footer.js
@@ -12,6 +12,7 @@ const Footer = () => {
         <img
           src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg'
           alt='TMDB logo'
+          width='100%'
         />
       </div>
     </div>
